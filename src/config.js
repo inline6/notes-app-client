1c050f3309641296ee493424c73381b396dfe390
mihai <bena.mihai@gmail.com>
2020-12-01 23:55:14 +0200

    Setting up React app

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+    s3: {
+      REGION: "eu-central-1",
+      BUCKET: "notes-app-upload-inline6",
+    },
+    apiGateway: {
+      REGION: "eu-central-1",
+      URL: "https://fqi5k6v5s7.execute-api.eu-central-1.amazonaws.com/prod",
+    },
+    cognito: {
+      REGION: "eu-central-1",
+      USER_POOL_ID: "eu-central-1_kJCGw02f8",
+      APP_CLIENT_ID: "h12fia5tg8deb75847uvbked8",
+      IDENTITY_POOL_ID: "eu-central-1:e49acbbf-f376-49bf-8028-40f856242e39",
+    },
+  };
+  
+  export default config;
